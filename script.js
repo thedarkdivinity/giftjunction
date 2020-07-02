d4988d10c0edd9e0f4bec065ddcdf8255b2b55c6
Sayush Kamat <thedarkdivinity@users.noreply.github.com>
2020-07-02 14:31:26 +0530

    client website completed

---
+++ script.js
@@ -0,0 +1,42 @@
+
+  AOS.init({
+    offset:400,
+    duration:1000
+  });
+
+
+
+
+
+
+const preloader=document.getElementById('loading');
+      const dis=()=>{
+        preloader.style.display='none';
+      }
+      const type=new Typed('#typed',{
+        stringsElement:'#typed-strings',
+        typeSpeed:20,
+        loop:true,
+        loopCount:80,
+        
+        
+
+      });
+      const type2=new Typed('#typ2op',{
+        stringsElement:'#typ2',
+        typeSpeed:40,
+        loop:true,
+        loopCount:80,
+        
+        
+
+      });
+      const type3=new Typed('#typ3op',{
+        stringsElement:'#typ3',
+        typeSpeed:40,
+        loop:true,
+        loopCount:80,
+        
+        
+
+      });
